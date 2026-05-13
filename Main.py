05bda967b000db8fa3e94fce7658f2afebb37810
jsaccone-star <juansasaccone@gmail.com>
2026-05-13 13:46:19 -0300

    mas cambios

--- Main.py
+++ Main.py
@@ -7,12 +7,10 @@ foto = input('Ingrese la ruta de su imagen: ')
 while not os.path.exists(foto):
     foto = input('Ingrese una ruta correcta: ')
 
-
 Tipo_imagen = input(f'Ingrese el tipo de imagen que quiere imprimir (ASCII / Pixel-ART): ')
 
 while Tipo_imagen != "ASCII" and Tipo_imagen != "Pixel-ART":
     Tipo_imagen = input(f'Ingrese un tipo de imagen valido: ')
-
 
 if Tipo_imagen == "ASCII":
     ASCIIprint.ASCIIprint(foto)
